af37fa6d98bf29291a6d0ea3deaa575436027f5f
zishahed <zishahed25@gmail.com>
2025-05-09 22:43:51 +0600

    Add array input and output test.cpp file

--- test.cpp
+++ test.cpp
@@ -1,5 +1,15 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main(){
-    cout << "testing my git skills\n";
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    cout << "The array elements: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << '\n';
 }
